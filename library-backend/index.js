bb77291f09f89ebddceab17a308b0343026de464
Student <student@sirafrica.org>
2026-08-10 10:43:35 +0300

    Add author filter to allBooks

--- library-backend/index.js
+++ library-backend/index.js
@@ -111,18 +111,24 @@ const typeDefs = `
     bookCount: Int!
   }
 
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-    allBooks: [Book!]!
-    allAuthors: [Author!]!
-  }
+type Query {
+  bookCount: Int!
+  authorCount: Int!
+  allBooks(author: String): [Book!]!
+  allAuthors: [Author!]!
+}
 `
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+  if (!args.author) {
+    return books
+  }
+
+  return books.filter(book => book.author === args.author)
+},
     allAuthors: () => authors,
   },
 
