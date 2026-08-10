601fe8574b880f8fd1f4a9227a2fc8bd4ac2ef06
Student <student@sirafrica.org>
2026-08-10 11:15:32 +0300

    Add genre filter to allBooks

--- library-backend/index.js
+++ library-backend/index.js
@@ -114,7 +114,7 @@ const typeDefs = `
 type Query {
   bookCount: Int!
   authorCount: Int!
-  allBooks(author: String): [Book!]!
+ allBooks(author: String, genre: String): [Book!]!
   allAuthors: [Author!]!
 }
 `
@@ -122,12 +122,22 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) => {
-  if (!args.author) {
-    return books
+ allBooks: (root, args) => {
+  let filteredBooks = books
+
+  if (args.author) {
+    filteredBooks = filteredBooks.filter(
+      book => book.author === args.author
+    )
   }
 
-  return books.filter(book => book.author === args.author)
+  if (args.genre) {
+    filteredBooks = filteredBooks.filter(
+      book => book.genres.includes(args.genre)
+    )
+  }
+
+  return filteredBooks
 },
     allAuthors: () => authors,
   },
