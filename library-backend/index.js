01ae28c5b880aa4e923b5cf03644b63ba4bc08ea
Student <student@sirafrica.org>
2026-08-10 10:16:02 +0300

    Implement allBooks query

--- library-backend/index.js
+++ library-backend/index.js
@@ -98,16 +98,24 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
-
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 }
 
