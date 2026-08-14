c956c39db6888f2de3589f760f2c101595393b3d
Student <student@sirafrica.org>
2026-08-14 10:35:35 +0300

    book genre part 1

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,21 +1,42 @@
+import { useState } from 'react'
+
 const Books = ({ show, books }) => {
+  const [genre, setGenre] = useState('patterns')
+
   if (!show) {
     return null
   }
+
+  const filteredBooks = genre
+    ? books.filter(book => book.genres.includes(genre))
+    : books
+
+  const genres = [
+    'refactoring',
+    'agile',
+    'patterns',
+    'design',
+    'crime',
+    'classic'
+  ]
 
   return (
     <div>
       <h2>books</h2>
 
+      <div>
+        in genre <b>{genre}</b>
+      </div>
+
       <table>
         <tbody>
           <tr>
-            <th>title</th>
+            <th></th>
             <th>author</th>
             <th>published</th>
           </tr>
 
-          {books.map((book) => (
+          {filteredBooks.map(book => (
             <tr key={book.id}>
               <td>{book.title}</td>
               <td>{book.author.name}</td>
@@ -24,6 +45,19 @@ const Books = ({ show, books }) => {
           ))}
         </tbody>
       </table>
+
+      {genres.map(g => (
+        <button
+          key={g}
+          onClick={() => setGenre(g)}
+        >
+          {g}
+        </button>
+      ))}
+
+      <button onClick={() => setGenre(null)}>
+        all genres
+      </button>
     </div>
   )
 }
